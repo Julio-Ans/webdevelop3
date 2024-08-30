7d7b6c323115ac2c636622fce6d64ad1c7c8c7b9
Julio-Ans <160240999+Julio-Ans@users.noreply.github.com>
2024-08-30 20:38:48 -0300

    Add files via upload

---
+++ SistemaLocadora/Cliente.py
@@ -0,0 +1,7 @@
+class Cliente:
+    def __init__(self, id, nome):
+        self.id=id
+        self.nome=nome
+    
+
+        
---
+++ SistemaLocadora/Filme.py
@@ -0,0 +1,9 @@
+
+class Filme:
+    def __init__(self, titulo, ano, genero):
+        self.titulo =titulo 
+        self.ano=ano
+        self.genero=genero 
+        self.disponibilidade = 'Disponível'
+
+ 
