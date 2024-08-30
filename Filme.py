7036f03bdd545e68dc478c704e6430c1accf4e35
Julio-Ans <160240999+Julio-Ans@users.noreply.github.com>
2024-08-30 20:39:26 -0300

    Delete Filme.py

--- Filme.py
+++
@@ -1,9 +0,0 @@
-
-class Filme:
-    def __init__(self, titulo, ano, genero):
-        self.titulo =titulo 
-        self.ano=ano
-        self.genero=genero 
-        self.disponibilidade = 'Disponível'
-
- 
